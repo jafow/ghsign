5c4a851731d2e797162d9471536466b6d3bcc1c8
jafow <jared.a.fowler@gmail.com>
2018-04-25 15:55:33 -0700

    handle username arg

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
-extern crate request;
+#[macro_use]
 extern crate clap;
+extern crate request;
 
-use std::env;
 use std::collections::HashMap;
 use clap::{Arg, App};
 
 fn main() {
+
     let matches = App::new("ghsign")
-                    .version("0.1.0")
-                    .arg(Arg::with_name("sign")
-                         .short("s")
-                         .long("sign")
-                         .value_name("sign"))
-                    .arg(Arg::with_name("verify")
-                         .short("r")
-                         .long("verify")
-                         .value_name("verify"))
-                    .get_matches();
+                        .version("0.1.0")
+                        .about("signs wth gh public key")
+                        .arg(Arg::with_name("sign")
+                             .short("s")
+                             .long("sign"))
+                        .arg(Arg::with_name("verify")
+                             .short("e")
+                             .long("verify"))
+                        .arg(Arg::with_name("username")
+                             .short("u")
+                             .long("user")
+                             .takes_value(true)
+                             .required(true))
+                        .get_matches();
 
+    let username = matches.value_of("username").unwrap();
+    let base_url = "https://github.com/";
+    let request_url = format!("{}{}.keys", base_url, username);
 
+    let mut headers: HashMap<String, String> = HashMap::new();
+    headers.insert("Connection".to_string(), "close".to_string());
 
-    if let Some(k) = matches.value_of("sign") {
-        println!("signer: {:?}", k);
+    let res = match request::get(&request_url, &mut headers) {
+        Ok(res) => res,
+        Err(err) => { println!("Error no keys: {}", err); return; }
     };
 
-    println!("Hello, world!");
 }
