00e4b1052abb9ee42062481e836fcd6c2780f8b2
jafow <jared.a.fowler@gmail.com>
2018-04-27 14:23:37 -0700

    curl for http client

--- lib/lib.rs
+++ lib/lib.rs
@@ -1,4 +1,8 @@
 extern crate reqwest;
+extern crate curl;
+
+use curl::easy::Easy;
+use std::io::{stdout, Write};
 
 pub struct GhSign {
     pub username: String
@@ -10,12 +14,15 @@ impl GhSign {
         let base_url = "https://github.com/";
         let request_url = format!("{}{}.keys", base_url, self.username);
 
-        let res = reqwest::get(request_url);
-        return res;
+        let mut easy = Easy::new();
+        easy.url(&request_url).unwrap();
+        easy.write_function(|data| {
+            Ok(stdout().write(data).unwrap())
+        }).unwrap();
+        easy.perform().unwrap();
     }
 
     pub fn sign (self) {
         let pk = self.fetch_key();
-        println!("pk : {:?}", pk);
     }
 }
